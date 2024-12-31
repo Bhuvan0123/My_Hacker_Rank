259515cc35dfc6c74768b54573eed7487a175571
Bhuvan0123 <bhuvanchandrap9@gmail.com>
2024-12-31 16:59:01 +0530

    31-12-2024

---
+++ Intro_To_Tutorial_Challenges.py
@@ -0,0 +1,8 @@
+# Complete the introTutorial function in the editor below. It must return an integer representing the zero-based index of .
+
+# introTutorial has the following parameter(s):
+
+# int arr[n]: a sorted array of integers
+# int V: an integer to search for
+def introTutorial(V, arr):
+    return arr.index(V)
