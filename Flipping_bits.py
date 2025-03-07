3ac86cda4130bac6fe5e09cc6ac7cd9b354e62a5
Bhuvan0123 <bhuvanchandrap9@gmail.com>
2025-03-07 19:32:58 +0530

    07-03-2025

---
+++ Flipping_bits.py
@@ -0,0 +1,4 @@
+# You will be given a list of 32 bit unsigned integers. Flip all the bits ( and ) and return the result as an unsigned integer.
+def flippingBits(n):
+    k=4294967295
+    return k^n
