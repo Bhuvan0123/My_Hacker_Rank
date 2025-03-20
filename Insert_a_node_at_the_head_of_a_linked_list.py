52a69d95053e99cd7be38dfdbad13afafa5b0800
Bhuvan0123 <bhuvanchandrap9@gmail.com>
2025-03-20 23:16:28 +0530

    20-03-2025

---
+++ Insert_a_node_at_the_head_of_a_linked_list.py
@@ -0,0 +1,5 @@
+# Given a pointer to the head of a linked list, insert a new node before the head. The  value in the new node should point to  and the  value should be replaced with a given value. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
+def insertNodeAtHead(llist, data):
+    node=SinglyLinkedListNode(data)
+    node.next=llist
+    return node
