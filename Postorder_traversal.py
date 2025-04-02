74c565ed7e6089cf2e68fa9389ff1ef6fe913080
Bhuvan0123 <bhuvanchandrap9@gmail.com>
2025-04-02 18:29:51 +0530

    02-04-2025

---
+++ Postorder_traversal.py
@@ -0,0 +1,6 @@
+# Complete the  function in the editor below. It received  parameter: a pointer to the root of a binary tree. It must print the values in the tree's postorder traversal as a single line of space-separated values.
+def postOrder(root):
+    if root:
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.info,end=" ")
